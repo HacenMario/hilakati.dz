cd7baea8acf261ef2c4f12745fd412e042165ed4
HacenMario <stevenhacen@gmail.com>
2026-07-12 17:29:40 +0100

    Add createdAt field to Notification schema
    Added createdAt field to Notification schema.

--- backend/models/Notification.js
+++ backend/models/Notification.js
@@ -1,4 +1,4 @@
-onst mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 const NotificationSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, required: true, index: true },
@@ -7,6 +7,7 @@ const NotificationSchema = new mongoose.Schema({
     message: { type: String, required: true },
     read: { type: Boolean, default: false },
     link: { type: String, default: '' },
-}, { timestamps: true });
+    createdAt: { type: Date, default: Date.now }
+});
 
 module.exports = mongoose.model('Notification', NotificationSchema);
