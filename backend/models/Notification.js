b2eca5f71e1f90a67598b8701a8bfa542b740880
HacenMario <stevenhacen@gmail.com>
2026-07-12 19:00:16 +0100

    Fix formatting of createdAt field in Notification model

--- backend/models/Notification.js
+++ backend/models/Notification.js
@@ -7,7 +7,7 @@ const NotificationSchema = new mongoose.Schema({
     message: { type: String, required: true },
     read: { type: Boolean, default: false },
     link: { type: String, default: '' },
-    createdAt: { type: Date, default: Date.now }
+    createdAt: { type: Date, default: Date.now },
     appointmentTime: { type: String, default: '' }
 });
 
