4719c3844639428328c9b5ac2786c98bb96184ff
HacenMario <stevenhacen@gmail.com>
2026-07-12 19:01:48 +0100

    Update Notification.js

--- backend/models/Notification.js
+++ backend/models/Notification.js
@@ -7,7 +7,6 @@ const NotificationSchema = new mongoose.Schema({
     message: { type: String, required: true },
     read: { type: Boolean, default: false },
     link: { type: String, default: '' },
-    createdAt: { type: Date, default: Date.now },
     appointmentTime: { type: String, default: '' }
 });
 
